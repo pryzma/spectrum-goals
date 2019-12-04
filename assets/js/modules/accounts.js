987c253fa626d454ebcb1248c27615bdca9820a9
Ans <ans1975@gmail.com>
2019-12-04 12:13:45 +0100

    simple list

--- assets/js/modules/accounts.js
+++ assets/js/modules/accounts.js
@@ -1,21 +1,43 @@
  /** accounts module object */
 const accounts = {
-    name : 'Accounts',
-    default : accountsOverview
-}
+  name : 'Accounts',
+  default : accountsOverview,
+  template : 'accounts'
+};
+
+const accountList = { data : [] };
+
+const accountDataModify = (account) => {
+  const accountAdd = {
+    name : account.firstName + ' ' +  account.lastName
+  };
+  return { ...account, ...accountAdd };
+};
+
  /** API data component object */
 const accountsData = {
-    url : 'api/accounts'
-}
+  url : 'api/accounts',
+  modify : accountDataModify,
+  callback : (data) => accountList.data = data
+};
+
+const accountListTableLabels = {
+  name : { label : 'Naam' },
+  username : { label : 'Gebruikersnaam' },
+  email : { label : 'E-Mail'}
+};
+
 /** Overview of accounts */
-function accountsOverview(){
-    /** Table component object */
-    const accountsOverviewTable = {
-        el : '#accountsOverview',
-        model : 'Account',
-        data : accountsData
-    }
-    return component.table(accountsOverviewTable)
-    
+function accountsOverview() {
+  /** Table component object */
+  const accountsListTable = {
+    el : '#accountsList',
+    model : 'Account',
+    class : 'table-striped table-hover',
+    cols : accountListTableLabels,
+    data : accountsData
+  };
+  return component.table(accountsListTable);
 }
-application.add('accounts',accounts);
+
+application.add('accounts', accounts);
