78184229aed9216c4737361faaceac3f1ab8f9cf
Christoffel Colombijn <ccolombijn@gmail.com>
2019-12-29 14:56:46 +0100

    Updated app/routes

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,11 @@ module.exports = (app)=>{
    console.log(`\x1b[36m[config.server.routes]\x1b[0m \x1b[3mconfig/app.json\x1b[0m`)
    for(const route in routes){
        app.use(route,require(`../routes/${routes[route]}`));
-       console.log('\x1b[36m',`[route]\x1b[0m \x1b[37mhttp://127.0.0.1:3000${route}\x1b[0m\x1b[36m : \x1b[3mroutes/${routes[route]}.js\x1b[0m`)
+       const port = process.env.REF_ADR === '127.0.0.1' ? `:${process.env.REF_HTTP_PORT }` : ''
+    
+        console.log('\x1b[36m',`[route]\x1b[0m \x1b[37m${process.env.REF_HTTP_PROTOCOL}://${process.env.REF_ADR}${port}${route}\x1b[0m\x1b[36m : \x1b[3mroutes/${routes[route]}.js\x1b[0m`)
+       
+      
     
     }
     
